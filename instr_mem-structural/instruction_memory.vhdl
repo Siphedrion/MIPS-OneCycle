2800457ba9d8b2a51818c0483b1e55c8713853c6
Eduardo Rodriguez-Martinez <erm@azc.uam.mx>
2023-10-10 13:00:18 -0600

    instruction memory modified
    Program "sort" hard wired in instruction memory

--- instr_mem-structural/instruction_memory.vhdl
+++ instr_mem-structural/instruction_memory.vhdl
@@ -30,58 +30,63 @@ begin
 
 --TODO: Make the generation of decoder lines automatically
 
-  instruction(31) <= dec_line(0) or dec_line(1) or dec_line(16);
-  instruction(30) <= '0';
-  instruction(29) <= dec_line(16);
-  instruction(28) <= dec_line(4) or dec_line(6) or dec_line(10) or
-                     dec_line(15);
-  instruction(27) <= dec_line(0) or dec_line(1) or dec_line(16);
-  instruction(26) <= dec_line(0) or dec_line(1) or dec_line(16);
-  instruction(25) <= dec_line(14);
-  instruction(24) <= dec_line(7) or dec_line(8) or dec_line(9) or
-                     dec_line(13);
-  instruction(23) <= '0';
-  instruction(22) <= dec_line(7) or dec_line(14);
-  instruction(21) <= dec_line(8) or dec_line(10) or dec_line(15);
-
-  instruction(20) <= dec_line(0) or dec_line(1) or dec_line(2) or 
-                     dec_line(3) or dec_line(5) or dec_line(7) or dec_line(8) or dec_line(13) or dec_line(16);
-  instruction(19) <= dec_line(9) or dec_line(11) or dec_line(12) or
-                     dec_line(14);
-  instruction(18) <= '0';
-  instruction(17) <= dec_line(1) or dec_line(7) or dec_line(11) or 
-                     dec_line(12) or dec_line(16);
-  instruction(16) <= dec_line(0) or dec_line(2) or
-                     dec_line(3) or dec_line(5) or dec_line(7) or
-                     dec_line(8) or dec_line(9) or dec_line(13) or dec_line(16);
-  instruction(15) <= dec_line(2) or dec_line(10) or dec_line(11) or 
-                     dec_line(15);
-  instruction(14) <= dec_line(3) or dec_line(5) or dec_line(7) or dec_line(8) or
-                     dec_line(10) or dec_line(12) or dec_line(13) or
-                     dec_line(15);
-  instruction(13) <= dec_line(10) or dec_line(15);
-  instruction(12) <= dec_line(2) or dec_line(7) or dec_line(10) or
-                     dec_line(11) or dec_line(12) or dec_line(15);
-  instruction(11) <= dec_line(2) or dec_line(5) or dec_line(8) or
-                     dec_line(9) or dec_line(10) or dec_line(11) or
+  instruction(31) <= dec_line( 1) or dec_line(10) or dec_line(11) or
                      dec_line(14) or dec_line(15);
-  instruction(10) <= dec_line(10) or dec_line(15);
-  instruction( 9) <= dec_line(10) or dec_line(15);
-  instruction( 8) <= dec_line(10) or dec_line(15);
-  instruction( 7) <= dec_line(10) or dec_line(15);
-  instruction( 6) <= dec_line(10) or dec_line(15);
-  instruction( 5) <= dec_line(2) or dec_line(3) or dec_line(5) or
-                     dec_line(7) or dec_line(8) or dec_line(9) or
-                     dec_line(10) or dec_line(11) or dec_line(12) or
-                     dec_line(13) or dec_line(14) or dec_line(15);
-  instruction( 4) <= dec_line(10) or dec_line(15);
-  instruction( 3) <= dec_line(4) or dec_line(9) or
-                     dec_line(10) or dec_line(14) or dec_line(16);
-  instruction( 2) <= dec_line(1) or dec_line(2) or dec_line(3) or
-                     dec_line(5) or dec_line(10) or dec_line(11) or
-                     dec_line(12) or dec_line(15);
-  instruction( 1) <= dec_line(6) or dec_line(9) or dec_line(14);
-  instruction( 0) <= dec_line(2) or dec_line(3) or dec_line(4) or dec_line(5) or
-                     dec_line(11) or dec_line(15);
+  instruction(30) <= '0';
+  instruction(29) <= dec_line( 0) or dec_line( 5) or dec_line( 6) or
+                     dec_line(14) or dec_line(15) or dec_line(16) or
+                     dec_line(18);
+  instruction(28) <= dec_line( 4) or dec_line( 7) or dec_line(13);
+  instruction(27) <= dec_line( 1) or dec_line( 6) or dec_line(10) or
+                     dec_line(11) or dec_line(14) or dec_line(15) or
+                     dec_line(17) or dec_line(19);
+  instruction(26) <= dec_line( 1) or dec_line( 7) or dec_line(10) or
+                     dec_line(11) or dec_line(14) or dec_line(15);
+  instruction(25) <= dec_line( 3) or dec_line( 5) or dec_line( 6) or
+                     dec_line( 9) or dec_line(16) or dec_line(18);
+  instruction(24) <= dec_line( 4) or dec_line( 7) or dec_line(10) or
+                     dec_line(11) or dec_line(12) or dec_line(13) or
+                     dec_line(14) or dec_line(15);
+  instruction(23) <= dec_line(12);
+  instruction(22) <= dec_line( 9) or dec_line(10) or dec_line(11) or
+                     dec_line(14) or dec_line(15);
+  instruction(21) <= dec_line( 0) or dec_line( 6) or dec_line(16);
+  instruction(20) <= dec_line( 0) or dec_line( 1) or dec_line( 3) or 
+                     dec_line( 5) or dec_line( 8) or dec_line(16) or
+                     dec_line(17) or dec_line(18) or dec_line(19);
+  instruction(19) <= dec_line( 6) or dec_line( 9) or dec_line(10) or
+                     dec_line(11) or dec_line(12) or dec_line(14) or
+                     dec_line(15);
+  instruction(18) <= dec_line(11) or dec_line(14);
+  instruction(17) <= dec_line( 0) or dec_line( 1) or dec_line( 3) or
+                     dec_line(10) or dec_line(12) or dec_line(15);
+  instruction(16) <= dec_line( 1) or dec_line( 3) or dec_line( 5) or
+                     dec_line( 8) or dec_line( 9) or dec_line(10) or
+                     dec_line(12) or dec_line(15) or dec_line(16);
+  instruction(15) <= dec_line( 2) or dec_line( 5) or dec_line(16);
+  instruction(14) <= dec_line( 3) or dec_line( 5) or dec_line( 8) or
+                     dec_line( 9) or dec_line(12) or dec_line(16);
+  instruction(13) <= dec_line( 5) or dec_line(16);
+  instruction(12) <= dec_line( 5) or dec_line( 9) or dec_line(16);
+  instruction(11) <= dec_line( 5) or dec_line( 8) or dec_line(16);
+  instruction(10) <= dec_line( 5) or dec_line(16);
+  instruction( 9) <= dec_line( 5) or dec_line(16);
+  instruction( 8) <= dec_line( 5) or dec_line(16);
+  instruction( 7) <= dec_line( 5) or dec_line( 8) or dec_line(16);
+  instruction( 6) <= dec_line( 5) or dec_line(16);
+  instruction( 5) <= dec_line( 2) or dec_line( 3) or dec_line( 5) or
+                     dec_line( 9) or dec_line(12) or dec_line(16) or
+                     dec_line(20);
+  instruction( 4) <= dec_line( 5) or dec_line(16);
+  instruction( 3) <= dec_line( 3) or dec_line( 4) or dec_line( 5) or
+                     dec_line( 7) or dec_line(12) or dec_line(16);
+  instruction( 2) <= dec_line( 0) or dec_line( 4) or dec_line( 5) or
+                     dec_line(11) or dec_line(13) or dec_line(15) or
+                     dec_line(16) or dec_line(17);
+  instruction( 1) <= dec_line( 3) or dec_line( 4) or dec_line( 5) or
+                     dec_line( 7) or dec_line(12) or dec_line(16) or
+                     dec_line(17) or dec_line(19);
+  instruction( 0) <= dec_line( 4) or dec_line( 5) or dec_line(16) or
+                     dec_line(18) or dec_line(19);
 
 end structural ; -- structural
